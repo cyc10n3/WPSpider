8b7e74c8ac2104a325345569d3ecec248d75a204
Gaurav Mishra <gmishra010@gmail.com>
2019-01-02 23:24:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(cookieParser());
 var hour = 3600000;
 app.use(session({
     key: 'user_sid',
-    secret: 'cyc10n3',
+    secret: 'Af*Vzgz(A48MnsurTDzCg*[$',
     resave: true,
     saveUninitialized: true,
     rolling: true,
